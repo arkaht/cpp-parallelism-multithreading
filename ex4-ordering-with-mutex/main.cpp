025fabd57eba5c09cb45c9bf42eb76c2b724bf54
Guthen <114919245+arkaht@users.noreply.github.com>
2023-10-24 18:42:08 +0200

    [finished] exercice 4

--- ex4-ordering-with-mutex/main.cpp
+++ ex4-ordering-with-mutex/main.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
 #include <thread>
+#include <semaphore>
 #include <mutex>
+#include <string>
 
+int x = 0;
 std::mutex mutex;
-const int MAX_N = 100;
+const int MAX_N = 1000;
 
-void thread_print_odd_numbers()
+void thread_odd_even( bool is_odd )
 {
-	for ( int i = 0; i <= MAX_N; i++ )
+	while ( x < MAX_N )
 	{
-		if ( i % 2 == 0 ) 
-		{
-			std::lock_guard guard( mutex );
-			continue;
-		}
+		std::lock_guard lock( mutex );
+		if ( x % 2 == is_odd ) continue;
 
-		std::cout << i << std::endl;
-	}
-}
-
-void thread_print_even_numbers()
-{
-	for ( int i = 0; i <= MAX_N; i++ )
-	{
-		if ( i % 2 != 0 )
-		{
-			std::lock_guard guard( mutex );
-			continue;
-		}
-
-		std::cout << i << std::endl;
+		std::cout << x << std::endl;
+		x++;
 	}
 }
 
 int main()
 {
-	std::thread thread0( thread_print_odd_numbers );
-	std::thread thread1( thread_print_even_numbers );
+	std::thread thread0( thread_odd_even, true );
+	std::thread thread1( thread_odd_even, false );
 
 	thread0.join();
 	thread1.join();
